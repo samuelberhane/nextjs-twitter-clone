5356e9c93ae50b90bcaf782e44c415c094fc765e
samuelberhane <1samibrhane@gmail.com>
2023-01-28 09:34:04 +0300

    Sign up user using email and password.

--- pages/auth/signup.jsx
+++ pages/auth/signup.jsx
@@ -2,18 +2,45 @@ import Image from "next/legacy/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "../../firebase/firebaseConfig";
+import { useRouter } from "next/router";
 
 const Signup = () => {
+  const router = useRouter();
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  // handle user sign up
   const handleSubmit = (e) => {
+    setLoading(true);
     e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        updateProfile(auth.currentUser, {
+          displayName: fullName,
+        });
+        const user = userCredential.user;
+        router.push("/");
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
   };
 
   return (
     <div className="px-6 md:px-10 lg:px-16">
+      {loading && (
+        <div className="fixed top-0 left-0 bottom-0 right-0 bg-[rgba(0,0,0,0.6)] z-20 flex items-center justify-center">
+          <img src="/img/loader.gif" alt="loader" />
+        </div>
+      )}
       <div className="flex justify-end bg-gradient-to-r from-green-500 to-black via-green-400 py-1 md:py-2">
         <div className="flex items-center justify-center">
           <Image
@@ -22,7 +49,7 @@ const Signup = () => {
             width={44}
             height={44}
             className="mb-4 scaleChange mr-12 md:mr-20 mt-5"
-          ></Image>
+          />
           <p className="text-white text-xl mr-2 md:mr-4">Language: English</p>
         </div>
       </div>
@@ -52,12 +79,20 @@ const Signup = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <p className="font-light mt-2 mb-5">
+          {error && (
+            <div className="sm:w-[350px] md:w-[450px] w-full flex justify-center mt-1">
+              <p className="text-center text-red-400">
+                {error.replace("Firebase: Error (auth/", "").replace(")", "")}
+              </p>
+            </div>
+          )}
+          <p className="font-light mt-2 mb-4">
             Already have an account?{" "}
             <Link href="/auth/login" className="text-blue-400">
               Login
             </Link>
           </p>
+
           <button
             type="submit"
             className="sm:w-[350px] md:w-[450px] w-full bg-blue-500 text-white py-2 rounded relative"
